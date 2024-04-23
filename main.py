61b93f36d87accc18e13fd44aaff599081e71295
xintaonian <johnnian@hotmail.com>
2024-04-23 22:21:09 +1000

    Creating options for users

---
+++ main.py
@@ -0,0 +1,12 @@
+
+print("Thanks to choosing us for your expenses recording!!!")
+running = True
+while running:
+    print("1. Recording expenses")
+    print("2. Editing Expenses")
+    print("3. Remove Expenses")
+    print("4. Review Expenses")
+    print("5. Exit")
+
+    choice = input("Please choose your option：")
+
